709fa9a7d83db1b9dcc4d50ea81f56fc48489da1
NicoCardenas <nicolas.cardenas@mail.escuelaing.edu.co>
2020-03-19 20:31:59 -0500

    finish

--- src/main/java/escuelaing/arep/spark/Spark.java
+++ src/main/java/escuelaing/arep/spark/Spark.java
@@ -2,33 +2,55 @@ package escuelaing.arep.spark;
 
 import static spark.Spark.*;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 public class Spark {
+
     public static void main(String[] args) {
+       
         getPort();
         staticFiles.location("/public");
         get("/api/v1/math/square", (req, res) -> {
-            return req.queryParams("value");
+            String value = req.queryParams("value");
+            String square = javaClient(value);
+            return square;
         });
 
-        options("/*",
-        (request, response) -> {
+        options("/*", (request, response) -> {
 
-            String accessControlRequestHeaders = request
-                    .headers("Access-Control-Request-Headers");
+            String accessControlRequestHeaders = request.headers("Access-Control-Request-Headers");
             if (accessControlRequestHeaders != null) {
-                response.header("Access-Control-Allow-Headers",
-                        accessControlRequestHeaders);
+                response.header("Access-Control-Allow-Headers", accessControlRequestHeaders);
             }
 
-            String accessControlRequestMethod = request
-                    .headers("Access-Control-Request-Method");
+            String accessControlRequestMethod = request.headers("Access-Control-Request-Method");
             if (accessControlRequestMethod != null) {
-                response.header("Access-Control-Allow-Methods",
-                        accessControlRequestMethod);
+                response.header("Access-Control-Allow-Methods", accessControlRequestMethod);
             }
 
             return "OK";
         });
+    }
+
+    static String javaClient(String value) throws MalformedURLException {
+        URL lambda = new URL("https://e4zmb8p2gj.execute-api.us-east-1.amazonaws.com/Beta?value=" + value);
+        String inputLine = null;
+        String res = "";
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(lambda.openStream()))) {
+            while ((inputLine = reader.readLine()) != null) {
+                res += inputLine;
+            }
+        } catch (IOException ex) {
+            Logger.getLogger(Spark.class.getName()).log(Level.SEVERE, "Not data get", ex);
+            System.err.println(ex);
+        }
+        return res;
     }
 
     /**
--- src/main/resources/public/index.js
+++ src/main/resources/public/index.js
@@ -1,13 +1,25 @@
 $(document).ready(function(){
+    $('#number').keypress(function (event) {
+        var keycode = (event.keyCode ? event.keyCode : event.which);
+        if (keycode == '13') {
+            event.preventDefault();
+            var str = $("#number").val();
+            $.get("http://localhost:4567/api/v1/math/square?value=" + str, function (data) {
+                    $("#res").html("El Cuadrado de " + str + " es: " + data);
+                    $("#number").val("");
+                }
+            );
+        }
+    });
     // Get value on button click and show alert
     $("#send").click(function(){
         var str = $("#number").val();
 
-        $.get("http://localhost:4567/api/v1/math/square?value=" + str, function(data) {
-               alert('page content: ' + data);
-            }
+        $.get("http://localhost:4567/api/v1/math/square?value=" + str, function (data) {
+            $("#res").html("El Cuadrado de " + str + " es: " + data);
+            $("#number").val("");
+        }
         );
 
-        $("#res").html(str);
     });
 });
